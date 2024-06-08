e52d00d35f3deb01c649fbe8782f06faebd99320
Mujahid2000 <your.email@example.com>
2024-06-08 17:46:53 +0600

    add some directory code

--- src/Page/Home/Projects.jsx
+++ src/Page/Home/Projects.jsx
@@ -1,42 +1,41 @@
 import { Link } from 'react-router-dom';
-import Data from '../../../public/Data.json';
 import { useEffect, useState } from 'react';
 
 const Projects = () => {
   const [project, setProject] = useState([]);
 
   useEffect(() => {
-    // Setting the imported data directly into the state
-    setProject(Data);
+    fetch('/Data.json')
+      .then(response => response.json())
+      .then(data => setProject(data))
+      .catch(error => console.error('Error fetching the data:', error));
   }, []);
-
 
   return (
     <div className='mt-10 place-items-center max-w-[1500px] mx-auto'>
-      <h1 className='text-center font-bold  text-4xl'>Projects</h1>
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 '>
+      <h1 className='text-center font-bold text-4xl'>Projects</h1>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
         {project.map(proj => (
           <div key={proj.id} className="max-w-sm bg-white mx-auto rounded-md overflow-hidden shadow-md hover:shadow-lg">
-          <div className="relative">
+            <div className="relative">
               <img className="w-96 h-64" src={proj.picture} alt="Product Image"/>
-              
-          </div>
-          <div className="p-4">
+            </div>
+            <div className="p-4">
               <h3 className="text-lg font-medium mb-2">{proj.title}</h3>
               <div className="flex items-center justify-between py-4">
-              <Link to={proj.live_link}>
-              <button className="bg-pink-500 hover:bg-pink-600 text-white font-bold py-2 px-4 rounded">
-              Live link
-            </button>
-              </Link>
-                 <Link to={`/projectDetails/${proj.id}`}>
-                 <button className="bg-pink-500 hover:bg-pink-600 text-white font-bold py-2 px-4 rounded">
-              Project Details
-            </button>
-                 </Link>
+                <Link to={proj.live_link}>
+                  <button className="bg-pink-500 hover:bg-pink-600 text-white font-bold py-2 px-4 rounded">
+                    Live link
+                  </button>
+                </Link>
+                <Link to={`/projectDetails/${proj.id}`}>
+                  <button className="bg-pink-500 hover:bg-pink-600 text-white font-bold py-2 px-4 rounded">
+                    Project Details
+                  </button>
+                </Link>
               </div>
+            </div>
           </div>
-      </div>
         ))}
       </div>
     </div>
